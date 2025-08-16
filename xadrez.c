9d8e2a05bdd73f035c5743515d19c60100fea72a
tatiam3 <tatiam3@yahoo.com.br>
2025-08-16 19:16:55 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,29 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+char xadrez {0} {0} = {
+{`T´, `C´, `B´, `Q´, `R´, `B´, `C´, `T´ },
+{`P´, `P´, `P´, `P´, `P´, `P´, `P´, `P´ },
+{` ´, ` ´, ` ´, ` ´, ` ´, ` ´, ` ´, ` ´ },
+{` ´, ` ´, ` ´, ` ´, ` ´, ` ´, ` ´, ` ´ },
+{` ´, ` ´, ` ´, ` ´, ` ´, ` ´, ` ´, ` ´ },
+{` ´, ` ´, ` ´, ` ´, ` ´, ` ´, ` ´, ` ´ },
+{`p´, `p´, `p´, `p´, `p´, `p´, `p´, `p´ },
+{`t´, `c´, `b´, `q´, `r´, `b´, `c´, `t´ },
+},
 
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
-
-    return 0;
+int pintartelatexto () {
+    system("CLS")
+int lin, col;
+    for (lin=0; <8; lin++) {
+        for (col=0; COL<8; col++} {
+printf("\xB2"); 
+}
+    printf("\n");
+}
+}
+int main()
+{
+    pintaTelaTexto();
+getch();
+return0;
 }
